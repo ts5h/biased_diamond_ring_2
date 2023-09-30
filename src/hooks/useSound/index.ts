94a941445711aa2c49d7fb303841b9014729b884
at4 <at4@jc4.so-net.ne.jp>
2023-09-30 13:09:05 +0200

    Update the useSound

--- src/hooks/useSound/index.ts
+++ src/hooks/useSound/index.ts
@@ -1,8 +1,10 @@
 import { useCallback } from "react";
+import * as Tone from "tone";
 
 export const useSound = () => {
   const playBeep = useCallback(() => {
-    console.log("play");
+    // const synth = new Tone.Synth().toDestination();
+    // synth.triggerAttackRelease("A#2", "64n");
   }, []);
 
   return {
